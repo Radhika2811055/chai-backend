b75d32e13e7613a0500e3e3be0356346ac1cc9e6
RADHIKA SAXENA <saxenaradhika49@gmail.com>
2026-05-23 22:37:56 +0530

    connecting app part-1

--- src/index.js
+++ src/index.js
@@ -7,4 +7,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    const server = app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+
+    server.on("error", (error) => {
+        console.log("ERR: ", error);
+        throw error;
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+})
 
